f4e644c3c6e950887acc652de244a42f4571fff2
Jacob Gillespie <jacobwgillespie@gmail.com>
2022-01-10 03:10:19 +0000

    Update tests and jsdocs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 export type Env = {[key: string]: string}
 export type OptionalEnv = {[key: string]: string | undefined}
 
+/** All of `process.env`, returns undefined if a variable is missing. */
 export const optional = new Proxy<OptionalEnv>(process.env, {
   get: (target, key: string) => {
     return target[key]
   },
 })
 
+/** All of `process.env`, throws an error if a variable is missing. */
 export const required = new Proxy<Env>(optional as Env, {
   get: (target, key: string) => {
     const value = target[key]
@@ -15,9 +17,19 @@ export const required = new Proxy<Env>(optional as Env, {
   },
 })
 
+/** Represents a required environment variable that was missing (not set). */
 export class MissingEnvironmentVariableError extends Error {
-  constructor(public variable: string) {
+  /** The name of the missing environment variable. */
+  variable: string
+
+  /**
+   * Represents a required environment variable that was missing (not set).
+   *
+   * @param variable The name of the missing environment variable.
+   */
+  constructor(variable: string) {
     super(`Missing required environment variable: ${variable}`)
     Error.captureStackTrace(this, MissingEnvironmentVariableError)
+    this.variable = variable
   }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,6 +4,25 @@ beforeAll(() => {
   process.env.EXAMPLE_VAR = 'example'
 })
 
+describe('env.optional', () => {
+  it('should return an environment variable', () => {
+    expect(env.optional.EXAMPLE_VAR).toBe('example')
+  })
+
+  it('should support key in operator', () => {
+    expect('EXAMPLE_VAR' in env.optional).toBe(true)
+  })
+
+  it('should return the keys of all environment variables', () => {
+    expect(Object.keys(env.optional)).toEqual(Object.keys(process.env))
+    expect(Object.getOwnPropertyNames(env.optional)).toEqual(Object.getOwnPropertyNames(process.env))
+  })
+
+  it('should not throw an error for missing optional environment variables', () => {
+    expect(() => env.optional.DOES_NOT_EXIST).not.toThrow()
+  })
+})
+
 describe('env.required', () => {
   it('should return an environment variable', () => {
     expect(env.required.EXAMPLE_VAR).toBe('example')
@@ -19,6 +38,13 @@ describe('env.required', () => {
   })
 
   it('should throw an error for missing required environment variables', () => {
-    expect(() => env.required.DOES_NOT_EXIST).toThrow()
+    expect(() => env.required.DOES_NOT_EXIST).toThrow(env.MissingEnvironmentVariableError)
+  })
+})
+
+describe('MissingEnvironmentVariableError', () => {
+  it('should store the name of the missing environment variable', () => {
+    const error = new env.MissingEnvironmentVariableError('EXAMPLE_VAR')
+    expect(error.variable).toBe('EXAMPLE_VAR')
   })
 })
